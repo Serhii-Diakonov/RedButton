76397f42068d3f215fa153b3974c096ed6e3360f
Serhii Diakonov <sergeidyakonov222@gmail.com>
2024-12-10 20:45:05 +0200

    Red button widget implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() {
   runApp(const RedButtonApp());
@@ -9,10 +13,53 @@ class RedButtonApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const CupertinoApp(
       title: 'Квестік',
       home: RedButtonView(title: 'Квестік'),
-      // debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Button extends StatefulWidget {
+  const Button({
+    super.key,
+    required this.imgPath,
+    required this.imgPathPressed,
+    required this.onParentPressed,
+  });
+
+  final String imgPath;
+  final String imgPathPressed;
+  final VoidCallback onParentPressed;
+
+  @override
+  State<Button> createState() => _ButtonState();
+}
+
+class _ButtonState extends State<Button> {
+  bool _pressed = false;
+
+  void _press(TapDownDetails details) {
+    setState(() {
+      _pressed = true;
+    });
+  }
+
+  void _release(TapUpDetails details) {
+    setState(() {
+      _pressed = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _press,
+      onTapUp: _release,
+      onTap: widget.onParentPressed,
+      behavior: HitTestBehavior.translucent,
+      child: Image.asset(_pressed ? widget.imgPathPressed : widget.imgPath),
     );
   }
 }
@@ -27,41 +74,54 @@ class RedButtonView extends StatefulWidget {
 }
 
 class _RedButtonViewState extends State<RedButtonView> {
-
   int _counter = 0;
+  var _states;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    loadAsset();
+  }
+
+  void _redButtonPressed() {
+    print("Button pressed");
     setState(() {
       _counter++;
     });
   }
 
+  Future<String> loadAsset() async {
+    var data = await rootBundle.loadString('assets/texts/states.json');
+    setState(() {
+      _states = json.decode(data);
+    });
+    return "ok";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
+    print(_states == null ? "" : _states[_counter]);
+    return CupertinoPageScaffold(
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            FloatingActionButton(
+              onPressed: _redButtonPressed,
+              child: Button(
+                  imgPath: 'imgs/red-btn.png',
+                  imgPathPressed: 'imgs/red-btn-p.png',
+                  onParentPressed: _redButtonPressed),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Container(
+              padding: const EdgeInsets.all(22),
+              child: Text(_states == null
+                  ? ''
+                  : _states[_counter]['buttonMessage'].toString()),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
